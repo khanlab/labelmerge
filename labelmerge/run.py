e9e089a639b6f4b57c71ad97b8a79145e9ea28ea
Tristan Kuehn <tkuehn@uwo.ca>
2022-10-24 11:29:26 -0400

    Fix multiple inputs warning

--- labelmerge/run.py
+++ labelmerge/run.py
@@ -3,9 +3,10 @@
 import os
 from snakebids.app import SnakeBidsApp
 
+
 def get_parser():
     """Exposes parser for sphinx doc generation, cwd is the docs dir"""
-    app = SnakeBidsApp('../labelmerge',skip_parse_args=True)
+    app = SnakeBidsApp("../labelmerge", skip_parse_args=True)
     return app.parser
 
 
