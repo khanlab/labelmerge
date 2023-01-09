146818f7a8f445f00c55514d42a13c36a74c2450
Tristan Kuehn <tkuehn@uwo.ca>
2023-01-09 13:04:14 -0500

    Add exclusions for base and overlay images

--- labelmerge/workflow/scripts/labelmerge.py
+++ labelmerge/workflow/scripts/labelmerge.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
+from __future__ import annotations
 from argparse import ArgumentParser
+from os import PathLike
 from pathlib import Path
 
 import nibabel as nib
 import numpy as np
+from numpy.typing import ArrayLike
 import pandas as pd
 import xarray as xr
 
 
-def load_atlas(atlas_path):
+def load_atlas(atlas_path: PathLike):
     """Loading relevant atlas data"""
     atlas = nib.load(atlas_path)
     data = atlas.get_fdata().astype(np.ushort)
@@ -22,9 +25,14 @@ class MetadataError(Exception):
     """Error raised when there's an issue with the metadata."""
 
 
-def assemble_mask(atlas, metadata, label, prefix=""):
+def assemble_mask(
+    atlas: ArrayLike,
+    metadata: pd.DataFrame,
+    label: int,
+    prefix: str = "",
+):
     try:
-        name = metadata[metadata["index"] == label].name.iloc[0]
+        name: str = metadata[metadata["index"] == label].name.iloc[0]
     except IndexError as err:
         raise MetadataError(
             f"Label with index {label} from {prefix}atlas not found in metadata table."
@@ -35,34 +43,73 @@ def assemble_mask(atlas, metadata, label, prefix=""):
     )
 
 
-def split_labels(atlas, metadata, prefix=""):
-    return xr.Dataset(
+def split_labels(
+    atlas: np.ndarray,
+    metadata: pd.DataFrame,
+    prefix: str = "",
+    exceptions: list[str] = [],
+) -> list[xr.Dataset]:
+    unique_vals = np.unique(atlas[atlas > 0])
+    normal_ds = xr.Dataset(
         dict(
             [
                 assemble_mask(atlas, metadata, label, prefix)
-                for label in np.unique(atlas[atlas > 0])
+                for label in unique_vals
+                if label not in exceptions
             ]
         )
     )
+    if not exceptions:
+        return [normal_ds]
+    exception_ds = xr.Dataset(
+        dict(
+            [
+                assemble_mask(atlas, metadata, label, prefix)
+                for label in unique_vals
+                if label in exceptions
+            ]
+        )
+    )
+    return [normal_ds, exception_ds]
 
 
-def merge_labels(base_ds, overlay_ds):
-    out_arr = np.zeros(list(dim for dim in base_ds.dims.values()), dtype=np.int_)
-    out_labels = list(range(len(base_ds) + len(overlay_ds)))
-    out_names = out_labels.copy()
+def merge_labels(datasets: list[xr.Dataset]):
+    """Merge several labelmaps into one output labelmap with metadata.
+
+    Parameters
+    ----------
+    datasets: list of Dataset
+        A list of datasets where each variable name is a label name and each
+        DataArray is the corresponding mask. Earlier entries in the list of
+        datasets are overwritten by later entries.
+
+    Returns
+    -------
+    ndarray
+        A 3D labelmap with the merged label data.
+    DataFrame
+        The metadata table containing label names.
+    """
+    out_arr: np.ndarray = np.zeros(
+        list(dim for dim in datasets[0].dims.values()), dtype=np.int_
+    )
+    out_labels = [
+        idx + 1 for idx in range(sum(len(dataset) for dataset in datasets))
+    ]
+    out_names = ["" for _ in out_labels]
 
     overall_idx = 0
-    for dataset in [base_ds, overlay_ds]:
+    for dataset in datasets:
         for name, arr in dataset.items():
             out_arr[arr] = int(overall_idx + 1)
-            out_labels[overall_idx] = int(overall_idx + 1)
             out_names[overall_idx] = name
             overall_idx += 1
     out_metadata = pd.DataFrame({"index": out_labels, "name": out_names})
     return out_arr, out_metadata
 
 
-def gen_parser():
+def gen_parser() -> ArgumentParser:
+    """Generate the CLI parser for this script."""
     parser = ArgumentParser()
     parser.add_argument("base_map")
     parser.add_argument("base_metadata")
@@ -70,6 +117,23 @@ def gen_parser():
     parser.add_argument("overlay_metadata")
     parser.add_argument("out_map_path")
     parser.add_argument("out_metadata_path")
+    parser.add_argument(
+        "--base_exceptions",
+        nargs="*",
+        help=(
+            "Space separated list of integer labels from the base image to keep over "
+            "overlay labels at the same voxels."
+        ),
+        type=int,
+    )
+    parser.add_argument(
+        "--overlay_exceptions",
+        nargs="*",
+        help=(
+            "Space separated list of integer labels from the overlay image to discard."
+        ),
+        type=int,
+    )
     return parser
 
 
@@ -80,11 +144,24 @@ def main():
     base_metadata = pd.read_csv(args.base_metadata, sep="\t")
     overlay_data, _, _ = load_atlas(Path(args.overlay_map))
     overlay_metadata = pd.read_csv(args.overlay_metadata, sep="\t")
-    base_ds = split_labels(base_data, base_metadata, prefix="base ")
-    overlay_ds = split_labels(
-        overlay_data, overlay_metadata, prefix="overlay "
+    base_exceptions = args.base_exceptions if args.base_exceptions else []
+    overlay_exceptions = args.overlay_exceptions if args.overlay_exceptions else []
+    base_datasets = split_labels(
+        base_data,
+        base_metadata,
+        prefix="base ",
+        exceptions=base_exceptions,
     )
-    merged_map, merged_metadata = merge_labels(base_ds, overlay_ds)
+    overlay_datasets = split_labels(
+        overlay_data,
+        overlay_metadata,
+        prefix="overlay ",
+        exceptions=overlay_exceptions,
+    )
+    # Note that overlay exceptions are ignored
+    merged_map, merged_metadata = merge_labels(
+        base_datasets[0:1] + overlay_datasets[0:1] + base_datasets[1:]
+    )
     merged_img = nib.Nifti1Image(
         dataobj=merged_map, affine=base_affine, header=base_header
     )
