0b7ecb5984d96b62e445421b04449378d45b9987
Arun Thurairajah <100954599+arunthurai@users.noreply.github.com>
2023-10-11 16:49:26 -0400

    Update run.py dynamic_args/black test

--- labelmerge/run.py
+++ labelmerge/run.py
@@ -9,7 +9,9 @@ from snakebids.cli import add_dynamic_args
 def get_parser():
     """Exposes parser for sphinx doc generation, cwd is the docs dir"""
     app = SnakeBidsApp("../labelmerge", skip_parse_args=True)
-    add_dynamic_args(app.parser, app.config["parse_args"], app.config["pybids_inputs"])
+    add_dynamic_args(
+        app.parser, app.config["parse_args"], app.config["pybids_inputs"]
+    )
     return app.parser
 
 
